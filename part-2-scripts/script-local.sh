2daac9212034cc7c3eb6bf68b8e3fd15fb697538
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2023-09-29 20:15:48 +0530

    Add action in Github

--- part-2-scripts/script-local.sh
+++ part-2-scripts/script-local.sh
@@ -1,1 +1,1 @@
-ssh -t -i "cert-file.cer" ubuntu@ec2-url  "sudo bash ~/deploy.sh"
+ssh -t -i "to-do-list-new.pem" ubuntu@ec2-13-48-5-119.eu-north-1.compute.amazonaws.com "sudo bash /home/ubuntu/deploy.sh"
